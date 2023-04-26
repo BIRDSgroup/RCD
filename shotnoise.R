0474ff1730d1b2aac6a2a5757b098de0958c08a3
BrinthaVP <51736151+BrinthaVP@users.noreply.github.com>
2023-04-26 12:50:19 +0530

    Update shotnoise.R

--- shotnoise.R
+++ shotnoise.R
@@ -15,7 +15,7 @@ p <- p + geom_line(aes(x=n, y=temp3), color='green') + geom_point(aes(x=n, y=tem
 
 p <- p + xlab('k') + ylab('Pr(x=k)') + xlim(0,30) + theme(panel.background = element_blank())
 p
-ggsave('poisson.svg', p)
+ggsave('output/poisson.svg', p)
 t# p <- ggplot(df) + geom_density( aes(x=temp1), color = 'red') 
 # p <- p + geom_density(aes(x=temp2), color='blue') 
 # p <- p + geom_density(aes(x=temp3), color='green')
